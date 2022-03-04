546796c832b07fcbc4850ff68458bc78d2978ac8
john-lay <john_lay@hotmail.co.uk>
2022-03-04 23:09:47 +0000

    Add inventory assets and logic

---
+++ assets/tilemaps/inventory.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="inventory" tilewidth="8" tileheight="8" tilecount="196" columns="14">
+ <image source="../tilesets/inventory.png" width="112" height="112"/>
+</tileset>
