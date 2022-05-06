231f88def569a4963aa5e5090e337420fa15cb62
john-lay <john_lay@hotmail.co.uk>
2022-05-06 20:17:05 +0100

    Add alternative map option to inventory screen

---
+++ assets/tilemaps/menu-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="menu-map" tilewidth="8" tileheight="8" tilecount="3" columns="3">
+ <image source="../tilesets/menu_map.png" width="24" height="8"/>
+</tileset>
