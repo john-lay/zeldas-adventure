cd1515bea12434786aeabdf04a3b606d51170ee7
john-lay <john_lay@hotmail.co.uk>
2023-01-07 20:34:17 +0000

    Visit the shipwreck on the South Seacoast

---
+++ assets/tilemaps/pirate-shipwreck.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="pirate-shipwreck" tilewidth="8" tileheight="8" tilecount="100" columns="10">
+ <image source="../tilesets/pirate-shipwreck.png" width="80" height="80"/>
+</tileset>
