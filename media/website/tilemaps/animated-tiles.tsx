8b9f01dd2959b2fee7b5aac471eb05227253fa2a
john-lay <john_lay@hotmail.co.uk>
2023-04-18 21:16:27 +0100

    Update website assets

---
+++ media/website/tilemaps/animated-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="animated-tiles" tilewidth="16" tileheight="16" tilecount="224" columns="4">
+ <image source="../tilesets/animated_tiles.w32.dmg.png" width="64" height="896"/>
+</tileset>
