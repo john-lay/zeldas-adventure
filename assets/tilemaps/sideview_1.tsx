b29ee0b76428a3f59dad2900995ff2451bf6625d
john-lay <john_lay@hotmail.co.uk>
2025-01-13 22:30:44 +0000

    Replace intro tower with Eagle Tower tiles

---
+++ assets/tilemaps/sideview_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="sideview_1" tilewidth="8" tileheight="8" tilecount="128" columns="16">
+ <image source="../tilesets/sideview_1.dmg.png" width="128" height="64"/>
+</tileset>
