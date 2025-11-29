8d83647e6e36d690168468c944db93345eda6d07
john-lay <john_lay@hotmail.co.uk>
2025-11-29 11:20:25 +0000

    Add French and German game over and save screens

---
+++ assets/tilemaps/game-over-fr.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="game-over-fr" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../tilesets/game-over-fr.png" width="160" height="144"/>
+</tileset>
